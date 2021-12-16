0ac0f26936a24b49565db848e38b807eca0670ae
Ryo <54506591+Ryo1018@users.noreply.github.com>
2021-12-17 08:43:22 +0900

    a

--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -75,7 +75,7 @@ namespace Snake
 
         private void StartGame(object sender, EventArgs e)
         {
-
+            RestartGame;
         }
 
         private void TakeSnapShot(object sender, EventArgs e)
@@ -90,12 +90,62 @@ namespace Snake
 
         private void UpdatePictureBoxGraphics(object sender, PaintEventArgs e)
         {
+            Graphics canvas = e.Graphics;
+
+            Brush snakeColour;
+
+            for(int i = 0; i < Snake.Count; i++)
+            {
+                if(i == 0)
+                {
+                    snakeColour = Brushes.Black;
+                }else
+                {
+                    snakeColour = Brushes.DarkGreen;
+                }
+
+                canvas.FillEllipse(snakeColour, new Rectangle
+                    (
+                    Snake[i].X * Settings.Width,
+                    Snake[i].Y * Settings.Height,
+                    Settings.Width, Settings.Height
+                    ));
+            }
+
+            canvas.FillEllipse(Brushes.DarkRed, new Rectangle
+                    (
+                    food.X * Settings.Width,
+                    food.Y * Settings.Height,
+                    Settings.Width, Settings.Height
+                    ));
 
         }
 
         private void RestartGame()
         {
+            maxWidth = picCanvas.Width / Settings.Width - 1;
+            maxHeight = picCanvas.Height / Settings.Height - 1;
 
+            Snake.Clear();
+
+            startButton.Enabled = false;
+            snapButton.Enabled = false;
+
+            score = 0;
+            txtScore.Text = "Score: " + score;
+
+            Circle head = new Circle { X = 10, Y = 5 };
+            Snake.Add(head); // adding the head part of the snake to the list
+
+            for(int i = 0; i < 10; i++)
+            {
+                Circle body = new Circle();
+                Snake.Add(body);
+            }
+
+            food = new Circle { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight) };
+
+            gameTimer.Start();
         }
 
         private void EatFood()
